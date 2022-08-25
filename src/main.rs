3afa3f3b60ad4a20e5d8c3e16a1a619aa1914ecf
Wesley Ryder <wr140815@ohio.edu>
2022-08-25 19:48:53 -0400

    tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
     println!("Hello, world!");
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
