1f315e6a6efdd17ba747da4201370b4d443c6ecc
Wesley Ryder <wr140815@ohio.edu>
2022-09-28 19:25:52 -0400

    events

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 // lib.rs
 use std::cell::RefCell;
 use std::rc::Rc;
-
 use std::io::stdout;
+use std::time::Duration;
 
-use app::App;
+use app::{App, AppReturn};
 use eyre::Result;
+use inputs::events::Events;
+use inputs::InputEvent;
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
 use crate::app::ui;
 
 pub mod app;
+pub mod inputs;
 
 pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
     // Configure Crossterm backend for tui
@@ -22,14 +25,28 @@ pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
     terminal.clear()?;
     terminal.hide_cursor()?;
 
+    // User event handler
+    let tick_rate = Duration::from_millis(200);
+    let events = Events::new(tick_rate);
+
     loop {
-        let app = app.borrow();
+        let mut app = app.borrow_mut();
+
         // Render
         terminal.draw(|rect| ui::draw(rect, &app))?;
-        // TODO handle inputs here
+
+        // Handle inputs
+        let result = match events.next()? {
+            InputEvent::Input(key) => app.do_action(key),
+            InputEvent::Tick => app.update_on_tick(),
+        };
+        // Check if we should exit
+        if result == AppReturn::Exit {
+            break;
+        }
     }
 
-    // Restore the terminal and close applicationi
+    // Restore the terminal and close application
     terminal.clear()?;
     terminal.show_cursor()?;
     crossterm::terminal::disable_raw_mode()?;
